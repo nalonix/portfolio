18ae1d43790574bf99ab6fe5ccfbc5131812e482
Naol Fekadu Hunduma <nf.naol9@gmail.com>
2025-02-26 12:18:00 +0300

    fixed analytics issue

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,14 +1,8 @@
 import type { Project } from '$lib/types';
 
-import { loglib } from '@loglib/tracker';
-
 export async function load({ fetch }) {
 	const response = await fetch('api/projects');
 	const projects: Project[] = await response.json();
-
-	loglib.record({
-		id: 'nalonix'
-	});
 
 	return {
 		projects
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,33 @@
 <script lang="ts">
-    // 
-    import * as config from '$lib/config'
-    // 
-    import Header from './_components/Header.svelte';
+	import { onMount } from 'svelte';
+	//
+	import * as config from '$lib/config';
+	import { loglib } from '@loglib/tracker';
+	//
+	import Header from './_components/Header.svelte';
 	import Hero from './_components/Hero.svelte';
 	import Footer from './_components/Footer.svelte';
-    import ProjectsSection from "./_components/ProjectsSection.svelte";
-    
-    interface Props {
-        data: any;
-    }
+	import ProjectsSection from './_components/ProjectsSection.svelte';
 
-    let { data }: Props = $props();
+	interface Props {
+		data: any;
+	}
+
+	onMount(() => {
+		// Loglib analytics
+		loglib.record({
+			id: 'nalonix'
+		});
+	});
+
+	let { data }: Props = $props();
 </script>
 
-
-
 <svelte:head>
-    <title>{config.title}</title>
+	<title>{config.title}</title>
 </svelte:head>
 
 <section>
-    <Hero />
-    <ProjectsSection projects={data.projects} />
+	<Hero />
+	<ProjectsSection projects={data.projects} />
 </section>
